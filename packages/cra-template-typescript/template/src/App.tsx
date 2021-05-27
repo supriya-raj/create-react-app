7e4adb42cfb47400a7da782a2efe08ba2555ed25
Supriya S <supriya.s@okcredit.in>
2021-05-27 11:15:00 +0530

    add new package

--- packages/cra-template-typescript/template/src/App.tsx
+++ packages/cra-template-typescript/template/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Switch from 'react-switch';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Switch checked={true} />
       </header>
     </div>
   );
